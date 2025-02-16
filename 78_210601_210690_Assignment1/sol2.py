934ca7907383e9fc33cf2a457d9e81b3f95646ed
Md Rahbar <mdrahbar21@iitk.ac.in>
2025-02-16 20:36:20 +0530

    add comments in sol2

--- 78_210601_210690_Assignment1/sol2.py
+++ 78_210601_210690_Assignment1/sol2.py
@@ -7,25 +7,34 @@ faulthandler.enable()
 
 DATA_PATH = os.path.join("Data", "Isabel_3D.vti")
 
+# Reading a .vti file and retrieving the image dataset
+
+
 def load_volume_data(file_path):
     reader = vtk.vtkXMLImageDataReader()
     reader.SetFileName(file_path)
     reader.Update()
     return reader.GetOutput()
 
+# Generating white bounding box outline using vtkOutlineFilter
+
+
 def create_outline(dataset):
     outline_filter = vtk.vtkOutlineFilter()
     outline_filter.SetInputData(dataset)
     outline_filter.Update()
-    
+
     mapper = vtk.vtkPolyDataMapper()
     mapper.SetInputConnection(outline_filter.GetOutputPort())
-    
+
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
     actor.GetProperty().SetColor(1, 1, 1)
-    
+
     return actor
+
+# Transfer Functions for Color and Opacity
+
 
 def configure_color_opacity():
     color_function = vtk.vtkColorTransferFunction()
@@ -39,7 +48,7 @@ def configure_color_opacity():
     ]
     for val, color in color_points:
         color_function.AddRGBPoint(val, *color)
-    
+
     opacity_function = vtk.vtkPiecewiseFunction()
     opacity_points = [
         (-4931.54, 1.0),
@@ -48,8 +57,11 @@ def configure_color_opacity():
     ]
     for val, opacity in opacity_points:
         opacity_function.AddPoint(val, opacity)
-    
+
     return color_function, opacity_function
+
+# Setting up Volume Properties and optional Phong Shading
+
 
 def setup_volume_properties(use_phong):
     color_tf, opacity_tf = configure_color_opacity()
@@ -57,50 +69,57 @@ def setup_volume_properties(use_phong):
     properties.SetColor(color_tf)
     properties.SetScalarOpacity(opacity_tf)
     properties.SetInterpolationTypeToLinear()
-    
+
     if use_phong:
         properties.ShadeOn()
         properties.SetAmbient(0.5)
         properties.SetDiffuse(0.5)
         properties.SetSpecular(0.5)
         properties.SetSpecularPower(10)
-    
+
     return properties
+
+# Mapping the volume data to the volume actor and setting up the renderer
+
 
 def setup_renderer(volume, outline):
     renderer = vtk.vtkRenderer()
     renderer.SetBackground(0.5, 0.5, 0.5)
     renderer.AddVolume(volume)
     renderer.AddActor(outline)
-    
+
     return renderer
+
 
 def render_scene(use_phong):
     dataset = load_volume_data(DATA_PATH)
     outline_actor = create_outline(dataset)
-    
+
     volume_mapper = vtk.vtkSmartVolumeMapper()
     volume_mapper.SetInputData(dataset)
-    
+
     volume = vtk.vtkVolume()
     volume.SetMapper(volume_mapper)
     volume.SetProperty(setup_volume_properties(use_phong))
-    
+
     renderer = setup_renderer(volume, outline_actor)
     render_window = vtk.vtkRenderWindow()
     render_window.SetSize(1000, 1000)
     render_window.AddRenderer(renderer)
-    
+
     interactor = vtk.vtkRenderWindowInteractor()
     interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
     interactor.SetRenderWindow(render_window)
-    
+
     render_window.Render()
     interactor.Start()
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="3D Volume Rendering with Phong Shading Option")
-    parser.add_argument("--phong", choices=["yes", "no"], default="no", help="Enable Phong shading (yes or no). Default is no.")
+    parser = argparse.ArgumentParser(
+        description="3D Volume Rendering with Phong Shading Option")
+    parser.add_argument("--phong", choices=["yes", "no"], default="no",
+                        help="Enable Phong shading (yes or no). Default is no.")
     args = parser.parse_args()
-    
+
     render_scene(args.phong == "yes")
